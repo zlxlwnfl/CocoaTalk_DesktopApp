fed569a47b240ad277f57d89fd325693917c61d3
zlxlwnfl@naver.com <zlxlwnfl@naver.com>
2023-05-29 23:59:25 +0900

    #1 material-ui, react-router 의존성 추가/로그인 샘플 페이지 추가

--- public/electron.js
+++ public/electron.js
@@ -1,3 +1,5 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
 const url = require('url');
 
 function createWindow() {
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MuiThemeProvider, createTheme } from '@material-ui/core';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const theme = createTheme();
+
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+ReactDOM.render(
+  <MuiThemeProvider theme={theme}>
+      <App/>
+  </MuiThemeProvider>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
